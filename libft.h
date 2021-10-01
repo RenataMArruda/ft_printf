107ec6e1fb26a9f7f4c0d61a7dd228d86c4e44b5
RenataMArruda <30669113+RenataMArruda@users.noreply.github.com>
2021-10-01 15:15:17 +0200

    Add files via upload

---
+++ libft.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        ::::::::            */
+/*   libft.h                                            :+:    :+:            */
+/*                                                     +:+                    */
+/*   By: rarruda <marvin@codam.nl>                    +#+                     */
+/*                                                   +#+                      */
+/*   Created: 2020/11/11 14:44:46 by rarruda       #+#    #+#                 */
+/*   Updated: 2021/09/30 17:46:52 by rarruda       ########   odam.nl         */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+# include <stddef.h>
+# include <unistd.h>
+
+int		ft_power(int p);
+void	ft_putchar(char c);
+int		ft_puthex(int nb, char id);
+void	ft_putnbr(int nb);
+int		ft_putpoi(unsigned long long pointer);
+void	ft_putstr(char *str);
+int		ft_putunsnbr(int nb);
+
+#endif
