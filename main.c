e71796a3d72dfabbef4de7928786e8b94982f218
Renata Arruda <rarruda@f1r2s7.codam.nl>
2022-10-11 13:01:26 +0200

    main

--- main.c
+++ main.c
@@ -16,5 +16,14 @@ int	main(void)
 	j = ft_printf("    mine: %x, %c, %p, %s, %d\n", a, a, b, c, d);
 	printf("count: %i\n\n", j);
 
+	int	e = 0;
+	printf("original: ");
+	i = printf("%x, %X ", e, e);
+	printf(" - count: %i\n", i);
+
+	ft_printf("mine: ");
+	j = ft_printf("%x, %X ", e, e);
+	printf(" - count: %i\n", j);
+
 	return(0);
 }
